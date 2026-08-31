0e92764cd80c05570e9a36b5ba61e5872ecca445
Ndimande-dev <ndimande@blackmanrossouw.co.za>
2026-08-31 10:48:51 +0200

    updated the scroll over layout to match current acf structure, plus css to match design

--- template-parts/layouts/layout-scroll_over_block.php
+++ template-parts/layouts/layout-scroll_over_block.php
@@ -1,32 +1,57 @@
 <?php
 
 $scroll_text = get_sub_field('scroll_over_text');
-$use_artworks = get_sub_field('use_artworks');
+//$use_artworks = get_sub_field('use_artworks');
+$use_artworks = get_sub_field('use_artwork_product=');
 $text_left = get_sub_field('text_on_the_left');
 $contain = get_sub_field('contain');
 $anchor = get_sub_field('anchor');
+$link = null;
 if($use_artworks){
 // Relationship field returns array (max 1)
     $artwork = get_sub_field('artwork');
     $artwork_post = $artwork[0];
     $artwork_id = $artwork_post->ID;
 
-    $image = get_field('artwork_image', $artwork_id);
-    $title = get_field('artwork_caption', $artwork_id);
-    $date  = get_field('art_date', $artwork_id);
+    $product_id = is_object( $artwork_post ) ? $artwork_post->ID : $artwork_post;
+    $image  = get_the_post_thumbnail_url( $product_id, 'large' );
+    $title           = get_the_title( $product_id );
+    $link            = get_permalink($product_id);
 
-    // artist relationship
-    $artist = get_field('artist_name', $artwork_id);
+    $artists = get_field( 'artists', $product_id );
 
-    if (is_array($artist)) {
-        $artist = $artist[0];
+    $artist_names = [];
+
+    // if (is_array($artists)) {
+    //     $artist = $artists[0];
+    // }
+
+    // $artist_name = $artist ? get_the_title($artist->ID) : null;
+    // $artist_link = $artist ? get_permalink($artist->ID) : null;
+
+    if ( ! empty( $artists ) ) {
+
+        foreach ( $artists as $artist ) {
+
+            $artist_names[] = is_object( $artist )
+                ? get_the_title( $artist->ID )
+                : get_the_title( $artist );
+        }
     }
 
-    $artist_name = $artist ? get_the_title($artist->ID) : null;
-    $artist_link = $artist ? get_permalink($artist->ID) : null;
+    if ( count( $artist_names ) > 1 ) {
+
+        $last_artist = array_pop( $artist_names );
+
+        $artist_output = implode( ', ', $artist_names ) . ' & ' . $last_artist;
+
+    } else {
+
+        $artist_output = $artist_names[0] ?? '';
+    }
 }
 else{
-    $title = get_sub_field('image_caption');
+    $title = get_sub_field('caption');
     $image = get_sub_field('image');
     $image = $image?wp_get_attachment_image_url($image, 'full'):null;
 } ?>
@@ -36,11 +61,8 @@ else{
     <div class="essay-artwork-image <?php echo $contain ? 'contain' : ''; ?> <?php echo $anchor ?: ''; ?>" style="background-image: url('<?php echo esc_url($image); ?>');" >
         <span class="screen-reader-text">
             <?php echo esc_html($title); ?>
-            <?php if ($artist_name) : ?>
-                <?php echo esc_html($artist_name); ?>
-            <?php endif; ?>
-            <?php if ($date) : ?>
-                <?php echo esc_html($date); ?>
+            <?php if (!empty($artist_output)) : ?>
+                <?php echo esc_html($artist_output); ?>
             <?php endif; ?>
         </span>
     </div>
@@ -53,19 +75,18 @@ else{
                     <?php echo apply_filters('the_content', $title); ?>
                 </div>
             <?php endif; ?>
-        <?php if ($artist_name) : ?>
-            <p class="artwork-artist">
-               <?php if ($artist_link) : ?>
-                <a class="artwork-title card_target" href="<?php echo esc_url($artist_link); ?>"><?php echo esc_html($artist_name); ?></a>
-            <?php else : ?> 
-                <?php echo esc_html($artist_name); ?>
+        
+            <?php if (!empty($artist_output)) : ?>
+                <p class="artwork-artist">
+                    <?php echo esc_html($artist_output); ?>
+                </p>
             <?php endif; ?>
-            </p>
-        <?php endif; ?>
 
-        <?php if ($date) : ?>
-            <p class="artwork-date"><?php echo esc_html($date); ?></p>
-        <?php endif; ?>
+            <?php if($link): ?>
+                <a class="artwork-link" href="<?php echo esc_url($link); ?>">
+                    <?php echo esc_html__('View Artwork', 'mbr'); ?>
+                </a>
+            <?php endif; ?>
 
     </div>
 
