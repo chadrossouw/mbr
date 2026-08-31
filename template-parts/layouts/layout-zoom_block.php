e02d527634ec6dfe9d0e53da0cb9866ceb95be79
Ndimande-dev <ndimande@blackmanrossouw.co.za>
2026-08-31 11:43:22 +0200

    updated the zooming block layout plus styles to match.

--- template-parts/layouts/layout-zoom_block.php
+++ template-parts/layouts/layout-zoom_block.php
@@ -3,7 +3,7 @@
 $zoom_anchor = get_sub_field('zoom_anchor_point');
 $non_zooming_text = get_sub_field('non_zooming_text');
 $zooming_text = get_sub_field('zooming_text');
-$use_artworks = get_sub_field('use_artworks');
+$use_artworks = get_sub_field('use_artwork_product');
 $text_left = get_sub_field('text_on_the_left');
 $zoom_id= null;
 if($use_artworks){
@@ -12,22 +12,45 @@ if($use_artworks){
     $artwork_post = $artwork[0];
     $artwork_id = $artwork_post->ID;
 
-    $image = get_field('artwork_image', $artwork_id);
-    $title = get_field('artwork_caption', $artwork_id);
-    $date  = get_field('art_date', $artwork_id);
+    $product_id = is_object( $artwork_post ) ? $artwork_post->ID : $artwork_post;
+    $image  = get_the_post_thumbnail_url( $product_id, 'large' );
+    $title           = get_the_title( $product_id );
+    $link            = get_permalink($product_id);
 
-    // artist relationship
-    $artist = get_field('artist_name', $artwork_id);
+    $artists = get_field( 'artists', $product_id );
 
-    if (is_array($artist)) {
-        $artist = $artist[0];
+    $artist_names = [];
+
+    // if (is_array($artist)) {
+    //     $artist = $artist[0];
+    // }
+
+    // $artist_name = $artist ? get_the_title($artist->ID) : null;
+    // $artist_link = $artist ? get_permalink($artist->ID) : null;
+
+    if ( ! empty( $artists ) ) {
+
+        foreach ( $artists as $artist ) {
+
+            $artist_names[] = is_object( $artist )
+                ? get_the_title( $artist->ID )
+                : get_the_title( $artist );
+        }
     }
 
-    $artist_name = $artist ? get_the_title($artist->ID) : null;
-    $artist_link = $artist ? get_permalink($artist->ID) : null;
+    if ( count( $artist_names ) > 1 ) {
+
+        $last_artist = array_pop( $artist_names );
+
+        $artist_output = implode( ', ', $artist_names ) . ' & ' . $last_artist;
+
+    } else {
+
+        $artist_output = $artist_names[0] ?? '';
+    }
 }
 else{
-    $title = get_sub_field('image_caption');
+    $title = get_sub_field('caption');
     $image = get_sub_field('image');
     $image = $image?wp_get_attachment_image_url($image, 'full'):null;
 } 
@@ -47,11 +70,8 @@ if($zoom_anchor=='coordinates'){
     <div class="essay-artwork-image <?php echo $zoom_anchor ?: ''; ?>" style="background-image: url('<?php echo esc_url($image); ?>');" >
         <span class="screen-reader-text">
             <?php echo esc_html($title); ?>
-            <?php if ($artist_name) : ?>
-                <?php echo esc_html($artist_name); ?>
-            <?php endif; ?>
-            <?php if ($date) : ?>
-                <?php echo esc_html($date); ?>
+            <?php if (!empty($artist_output)) : ?>
+                <?php echo esc_html($artist_output); ?>
             <?php endif; ?>
         </span>
     </div>
@@ -72,16 +92,12 @@ if($zoom_anchor=='coordinates'){
                 </div>
         <?php  endif; ?>
 
-        <?php if ($artist_name) : ?>
-            <?php if ($artist_link) : ?>
-                <p class="artwork-artist"><a class="card_target" href="<?php echo esc_url($artist_link); ?>"><?php echo esc_html($artist_name); ?></a></p>
+        <?php if ($artist_output) : ?>
+            <?php if ($link) : ?>
+                <p class="artwork-artist"><a class="card_target" href="<?php echo esc_url($link); ?>"><?php echo esc_html($artist_name); ?></a></p>
             <?php else : ?>
-            <p class="artwork-artist"><?php echo esc_html($artist_name); ?></p>
+            <p class="artwork-artist"><?php echo esc_html($artist_output); ?></p>
             <?php endif; ?>
-        <?php endif; ?>
-
-        <?php if ($date) : ?>
-            <p class="artwork-date"><?php echo esc_html($date); ?></p>
         <?php endif; ?>
 
     </div>
